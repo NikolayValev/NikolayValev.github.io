85d83f551bddf0756488a939e963e0c3dc001b6d
NikolayValev <nikolaivalev@gmail.com>
2022-08-13 14:17:16 -0400

    Deploy React app to GitHub Pages

---
+++ precache-manifest.482957c2d4b157ed56f41d40543077f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e32350b7faa954e21cd7a6b8f0232af2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cb151af013a34162a15",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "6fbae0b28482e53a2817",
+    "url": "/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "9cb151af013a34162a15",
+    "url": "/static/js/2.3e21f37f.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.3e21f37f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fbae0b28482e53a2817",
+    "url": "/static/js/main.d457fe63.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
